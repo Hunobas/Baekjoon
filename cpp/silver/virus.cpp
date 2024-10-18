71f001af66e91084e77e72e5fc29db23e63e713c
박태훈 <po127992@gmail.com>
2024-10-18 22:57:59 +0900

    10182024

---
+++ cpp/silver/virus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    int computers, connections;
+    cin >> computers >> connections;
+    cout << computers << ' ' << connections << endl;
+
+    return 0;
+}
